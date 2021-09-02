b4ed2cec0de987deea0e1ab7d2d3814e40f1ec38
Ganjang-chichen <59672593+Ganjang-chichen@users.noreply.github.com>
2021-09-02 23:37:15 +0900

    shell script

---
+++ 02리눅스시스템이해/script/233argument.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "shell script argument tutorial"
+
+# Argument
+
+echo "this script file \$0 = $0"
+echo "Argument Count \$# = $#"
+echo "Argument List \$1 = $1"
+echo "Argument List \$2 = $2"
+echo "Argument List \$3 = $3"
+
+echo "process ID is $$"
